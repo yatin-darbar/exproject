d27f193a02ed94f3c13f061b70cff0c48ec8b225
Yatin Darbar <yatin_darbar@hotmail.co.uk>
2021-03-15 11:04:37 +0000

    Create and Add Sine plot

---
+++ src/sine.py
@@ -0,0 +1,13 @@
+#########
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,2*(np.pi),100)
+y = np.sin(x)
+
+plt.plot(x,y)
+plt.title("Sine Curve")
+plt.xlabel("x")
+plt.ylabel("f(x)")
+
+plt.show()
